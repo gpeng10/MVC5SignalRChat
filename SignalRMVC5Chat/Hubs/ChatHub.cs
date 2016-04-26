22bcd5118d2cc18907a51fa63e57361058676413
Gary Peng <gpeng10@gmail.com>
2016-04-26 08:01:58 -0400

    first setup

---
+++ SignalRMVC5Chat/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRMVC5Chat
+{
+    public class ChatHub : Hub
+    {
+        static int _connectionCounter =0;
+
+        public void Hello()
+        {
+            Clients.All.hello();
+            
+        }
+
+        public void recordHit()
+        {
+            _connectionCounter += 1;
+            Clients.All.showCounter(_connectionCounter);
+        }
+
+        public void Send(string name, string message)
+        {
+            
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            _connectionCounter -= 1;
+            Clients.All.showCounter(_connectionCounter);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ SignalRMVC5Chat/Hubs/Startup.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Owin;
+using Microsoft.Owin;
+[assembly:OwinStartup(typeof(SignalRMVC5Chat.Startup))]
+
+namespace SignalRMVC5Chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
